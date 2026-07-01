66dfa4484a9ad0fc4e38ba5525f4decd9812a7d0
Yasemin Türkoğlu <126945023+fizikzedenin-kodlari@users.noreply.github.com>
2026-07-01 13:00:32 +0300

    Update 2.BASICS-2.1 Calculating.lean

--- 2.BASICS-2.1 Calculating.lean
+++ 2.BASICS-2.1 Calculating.lean
@@ -101,10 +101,10 @@ example (a b c d : ℝ) (hyp : c = d * a + b) (hyp' : b = a * d) : c = 2 * a * d
 ----------------------------
 example : c * b * a = b * (a * c) := by
   ring
-
+----------------------------
 example : (a + b) * (a + b) = a * a + 2 * (a * b) + b * b := by
   ring
-
+----------------------------
 example : (a + b) * (a - b) = a ^ 2 - b ^ 2 := by
   ring
 ----------------------------
