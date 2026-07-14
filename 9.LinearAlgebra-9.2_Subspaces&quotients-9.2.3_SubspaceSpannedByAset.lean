af3acb7346eb668ee5aaaa13efad03ff94a64436
Yasemin Türkoğlu <126945023+fizikzedenin-kodlari@users.noreply.github.com>
2026-07-14 14:55:13 +0300

    Update 9.LinearAlgebra-9.2_Subspaces&quotients-9.2.3_SubspaceSpannedByAset.lean

--- 9.LinearAlgebra-9.2_Subspaces&quotients-9.2.3_SubspaceSpannedByAset.lean
+++ 9.LinearAlgebra-9.2_Subspaces&quotients-9.2.3_SubspaceSpannedByAset.lean
@@ -1,7 +1,7 @@
 import Mathlib.Tactic
 --9.Linear Algebra
 --9.2. Subspaces and quotients
---9.2.3 Bir küme tarafından gerilen alt uzay (Subspace spanned by a set)
+--9.2.3 Subspace spanned by a set
 variable {K : Type*} [Field K] {V : Type*} [AddCommGroup V] [Module K V]
 -----------------------------------------------------------
 example {s : Set V} (E : Submodule K V) : Submodule.span K s ≤ E ↔ s ⊆ E :=
